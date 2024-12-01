014cf22df67da310ad17bd1a01cd6ec3fc5da28c
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-11-30 21:51:52 -0600

    Ignore build errors.

--- neo-sync/next.config.mjs
+++ neo-sync/next.config.mjs
@@ -2,8 +2,12 @@
 const nextConfig = {
   typescript: {
     // ⚠️ Warning: This allows production builds to successfully complete even if
-    // your project has TypeScript errors.
+    // your project has type errors.
     ignoreBuildErrors: true,
+  },
+  // Also ignore ESLint errors during build
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ["cdn.tiny.cloud"],
